dd1f06509672ad04a52142bbcdd319fa2de32e8a
indiest <indiestian@gmail.com>
2022-05-24 17:45:23 +0800

    Added missing AddPlayerProxyMessage

---
+++ Assets/channeld/MirrorMessages.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Channeld
+{
+    /// <summary>
+    /// When a player subs to multiple channels that need to create the player prefab in order to call RPC,
+    /// we need to bind the same netId with the player objects. 
+    /// The netId is created by the first channel that the player subs (usually the Global channel).
+    /// We call the player object on the server owns the first channel 'Authority Player' and other player objects 'Proxy Player'.
+    /// </summary>
+    public struct AddPlayerProxyMessage : NetworkMessage
+    {
+        public uint netId;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+    }
+}

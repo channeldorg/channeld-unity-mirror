033f0aa0833a4d647e9567dad35fa2086332b8a9
indiest <indiestian@gmail.com>
2022-06-27 14:40:56 +0800

    Added quotes to paths with CHANNELD_PATH in case that the path has space character

--- protogen.bat
+++ protogen.bat
@@ -1,4 +1,4 @@
-protoc --csharp_out=./Assets/channeld -I %CHANNELD_PATH%\pkg\channeldpb channeld.proto
-protoc --csharp_out=./Assets/channeld -I %CHANNELD_PATH%\pkg\channeldpb unity_common.proto
-protoc --csharp_out=./Assets/channeld/Examples/Tanks/Scripts -I %CHANNELD_PATH% -I %CHANNELD_PATH%\examples\unity-mirror-tanks\tankspb tanks.proto
+protoc --csharp_out=./Assets/channeld -I "%CHANNELD_PATH%\pkg\channeldpb" channeld.proto
+protoc --csharp_out=./Assets/channeld -I "%CHANNELD_PATH%\pkg\channeldpb" unity_common.proto
+protoc --csharp_out=./Assets/channeld/Examples/Tanks/Scripts -I "%CHANNELD_PATH%" -I "%CHANNELD_PATH%\examples\unity-mirror-tanks\tankspb" tanks.proto
 pause
